3cf37e5d1275a0ef4e9f2ccbec0b9164ba8658da
nasjcodes <5616230+nasjcodes@users.noreply.github.com>
2018-10-30 20:56:40 +0800

    Did some refactoring Put all strings into strings.xml (res) for better maintainability

--- app/src/main/java/quotebook/nasjcodes/com/quotebook/QuoteBook.java
+++ app/src/main/java/quotebook/nasjcodes/com/quotebook/QuoteBook.java
@@ -7,6 +7,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class QuoteBook extends Activity {
     int count = 0;
@@ -21,51 +22,61 @@ public class QuoteBook extends Activity {
         final TextView quoteText = findViewById(R.id.quote);
         final TextView personText = findViewById(R.id.person);
 
-        //Set initial prompt to tap for quotes
-        quoteText.setText("Tap screen for quotes");
-        personText.setText("nasjcodes");
+        //Get and set start quote
+        setQuote(getString(R.string.start_quote), quoteText, personText);
 
+        //Get end quote
+        final Quote endQuote = stringToQuote(getString(R.string.end_quote));
 
         //List of quotes
         final ArrayList<Quote> quoteList = new ArrayList<Quote>();
+        String[] texts = getResources().getStringArray(R.array.quotes_array);
+        addQuotes(quoteList, texts);
 
-        Quote quote4 = new Quote("You're more of a fun vampire. You don't suck blood, you just suck.", "Troy Barnes");
-        quoteList.add(quote4);
-
-        Quote quote1 = new Quote("Cool Beans", "Rod Kimble");
-        quoteList.add(quote1);
-
-        Quote quote2 = new Quote("How can mirrors be real if our eyes aren't real", "Jaden Smith");
-        quoteList.add(quote2);
-
-        Quote quote3 = new Quote("That's like me blaming owls for how bad I suck at analogies.", "Britta Perry");
-        quoteList.add(quote3);
-
-        Quote quote5 = new Quote("I was gonna be the first person in my family to graduate from community college. Everyone else graduated from normal college", "Troy Barnes");
-        quoteList.add(quote5);
-
-        //End quote
-        Quote quote6 = new Quote("End! Tap to begin again", "nasjcodes");
-        quoteList.add(quote6);
 
 
         touch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if (count >= quoteList.size()) {
+                if (count < quoteList.size()) {
+
+                    setQuote(quoteList.get(count), quoteText, personText);
+                    count++;
+
+                } else {
+
+                    setQuote(endQuote, quoteText, personText);
                     count = 0;
+
                 }
-
-                Quote q = quoteList.get(count);
-
-                quoteText.setText(q.getQuote());
-
-                personText.setText(q.getPerson());
-
-                count = count + 1;
 
             }
         });
+    }
+
+    private Quote stringToQuote(String text) {
+        String[] splitQuote = text.split("\\|");
+
+        return new Quote(splitQuote[0], splitQuote[1]);
+    }
+
+    private void setQuote(Quote quote, TextView quoteText, TextView personText) {
+        quoteText.setText(quote.getQuote());
+        personText.setText(quote.getPerson());
+    }
+
+    private void setQuote(String text, TextView quoteText, TextView personText) {
+        Quote quote = stringToQuote(text);
+        setQuote(quote, quoteText, personText);
+    }
+
+
+    private void addQuotes(List<Quote> quoteList, String[] texts) {
+
+        for(String text : texts) {
+            quoteList.add(stringToQuote(text));
+        }
+
     }
 }
